634b1bb3a7b0ada5fb3c22a404c547d65c1b6ac6
Jada Alexander <j.alexandertech@outlook.com>
2022-06-18 17:39:02 -0700

    add frontend and backend

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from 'aws-amplify';
-import config from "./aws-exports";
+import aws_exports from './aws-exports'
 
-Amplify.configure(config)
+Amplify.configure(aws_exports);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
